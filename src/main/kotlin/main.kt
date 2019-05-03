76331cf02d1db7228b3cd50f1fbffdf682738a98
Nathan Kessler <nathant93@gmail.com>
2019-05-02 20:59:43 -0400

    Parse messages from server

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,4 @@
+
 import io.ktor.network.selector.ActorSelectorManager
 import io.ktor.network.sockets.aSocket
 import io.ktor.network.sockets.openReadChannel
@@ -18,7 +19,7 @@ val logger = KotlinLogging.logger {}
 @KtorExperimentalAPI
 fun main() = runBlocking<Unit> {
     val port = 6667
-    val hostname = "localhost"
+    val hostname = "irc.freenode.net"
 
     val exec = Executors.newCachedThreadPool()
     val selector = ActorSelectorManager(exec.asCoroutineDispatcher())
@@ -38,10 +39,88 @@ fun main() = runBlocking<Unit> {
     while (!input.isClosedForRead) {
         logger.info("Reading...")
 
-        val response = input.readUTF8Line()
+        val response = input.readUTF8Line() ?: throw java.lang.RuntimeException("idk")
+        parseMessage(response)
         logger.info("Server said: '$response'")
-        if (response?.startsWith("PING") == true) {
+        if (response.startsWith("PING")) {
             output.write(response.replace("PING", "PONG") + "\r\n", Charsets.US_ASCII)
         }
     }
+}
+
+var position = 0
+
+fun parseMessage(message: String) {
+    logger.info("Parsing message: " + message)
+    var prefix = ""
+    var command: String?
+
+    position = 0
+    if (message[position] == ':') {
+        position++ // consume ':'
+        prefix = parsePrefix(message)
+        position++ // consume space
+    }
+    command = parseCommand(message)
+    while (position != message.length - 1 && message[position+1] != '\r') {
+        logger.info("Found param: " + parseParam(message))
+    }
+    // parse params while not crlf next
+
+    logger.info("'$prefix' '$command' '${message.slice(position until message.length)}'")
+}
+
+fun parseParam(message: String): String {
+    if (message[position] != ' ') throw RuntimeException("No space")
+    position++ // consume space
+
+    var middle = ""
+    if (isNoSpCrLfCl(message)) {
+        middle += message[position]
+        position++
+        while ((isNoSpCrLfCl(message) || message[position] == ':') && position != message.length - 1) {
+            middle += message[position]
+            position++
+        }
+    } else if (message[position] == ':') {
+        position++
+        while ((isNoSpCrLfCl(message) || message[position] == ':' || message[position] == ' ') && position != message.length - 1) {
+            middle += message[position]
+            position++
+        }
+    }
+    return middle
+}
+
+fun isNoSpCrLfCl(message: String): Boolean =
+        !listOf('\r', '\n', ' ', ':', '\u0000').contains(message[position])
+
+fun parsePrefix(message: String): String {
+    val end = message.findNextAfter(position, ' ') ?: throw RuntimeException("missing space")
+    val prefix = message.slice(position until end)
+    position = end
+    return prefix
+}
+
+fun parseCommand(message: String): String {
+    var command = ""
+    if (message[position] in 'A'..'Z' || message[position] in 'a'..'z') {
+        while (message[position] in 'A'..'Z' || message[position] in 'a'..'z') {
+            command += message[position++]
+        }
+    } else if (message[position] in '0'..'9') {
+        while (message[position] in '0'..'9') {
+            command += message[position++]
+        }
+    } else {
+        throw RuntimeException("No command")
+    }
+    return command
+}
+
+fun String.findNextAfter(start: Int, toFind: Char): Int? {
+    for (i in start until this.length) {
+        if (this[i] == toFind) return i
+    }
+    return null
 }
