6e12268d3c6bc6db64d37b0c97eb25d40a2c6d89
Nathan Kessler <nathant93@gmail.com>
2019-05-04 14:14:20 -0400

    Create KotBot class

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -12,33 +12,59 @@ var registered = false
 @ExperimentalIoApi
 @KtorExperimentalAPI
 fun main() = runBlocking {
-    val connection = IrcConnection.connect(
-            hostname = "localhost",
-            port = 6667
-    )
-
-    while (!connection.isClosedForRead()) {
-        logger.info("Reading...")
-
-        val response = MessageParser(connection.readLine()).parse()
-        logger.info("Server said: '$response}'")
-        eventHandler(connection, response)
-    }
+    KotBot.create()
 }
 
 @KtorExperimentalAPI
-suspend fun eventHandler(connection: IrcConnection, event: ServerMessage) {
-    if (event.command == "PING") {
-        val pong = "PONG ${event.parameters.first()}"
-        connection.write(pong)
+class KotBot private constructor (
+        private val connection: IrcConnection
+) {
+    companion object {
+        suspend fun create() {
+            val connection = IrcConnection.connect(
+                    hostname = "localhost",
+                    port = 6667
+            )
+            val kotbot = KotBot(connection)
+            kotbot.connect()
+            kotbot.run()
+        }
     }
-    if (event.command == "001") {
-        connection.write("JOIN #discodevs\r\n")
-        connection.write("PRIVMSG #discodevs :Hello from raw sockets in kotlin\r\n")
+
+    private suspend fun connect() {
+        while (!connection.isClosedForRead()) {
+            logger.info("Reading...")
+
+            val response = MessageParser(connection.readLine()).parse()
+            logger.info("Server said: '$response}'")
+            eventHandler(connection, response)
+        }
     }
-    if (event.command == "NOTICE" && !registered) {
-        registered = true
-        connection.write("NICK kotbot\r\n")
-        connection.write("USER kotbot 0 * :kotbot\r\n")
+
+    private suspend fun run() {
+        while (!connection.isClosedForRead()) {
+            logger.info("Reading...")
+
+            val response = MessageParser(connection.readLine()).parse()
+            logger.info("Server said: '$response}'")
+            eventHandler(connection, response)
+        }
+    }
+
+    private suspend fun eventHandler(connection: IrcConnection, event: ServerMessage) {
+        if (event.command == "PING") {
+            val pong = "PONG ${event.parameters.first()}"
+            connection.write(pong)
+        }
+        if (event.command == "001") {
+            connection.write("JOIN #discodevs\r\n")
+            connection.write("PRIVMSG #discodevs :Hello from raw sockets in kotlin\r\n")
+        }
+        if (event.command == "NOTICE" && !registered) {
+            registered = true
+            connection.write("NICK kotbot\r\n")
+            connection.write("USER kotbot 0 * :kotbot\r\n")
+        }
     }
 }
+
