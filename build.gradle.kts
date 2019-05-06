3ad88d402b5f042b9c06371f6da526a4e1483c99
Nathan Kessler <nathant93@gmail.com>
2019-05-05 22:57:17 -0400

    Clean up gradle file

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,23 +5,24 @@ plugins {
 }
 group = "us.kesslern"
 version = "1.0-SNAPSHOT"
-val ktor_version = "1.1.4"
 
 dependencies {
     compile("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.2.1")
-    compile("io.ktor:ktor-network:$ktor_version")
+    compile("io.ktor:ktor-network:1.1.4")
     compile("io.github.microutils:kotlin-logging:1.6.24")
-    val logback           = "1.2.+"
-    compile("ch.qos.logback:logback-classic:$logback")
+    compile("ch.qos.logback:logback-classic:1.2.+")
     implementation(kotlin("stdlib-jdk8"))
 }
+
 repositories {
     mavenCentral()
 }
+
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
     jvmTarget = "1.8"
 }
+
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
     jvmTarget = "1.8"
