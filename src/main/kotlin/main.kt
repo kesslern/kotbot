20ae00b8e2db1b426fc50de64a8047bf9d0d044a
Nathan Kessler <nathant93@gmail.com>
2019-05-07 22:30:23 -0400

    Cleanup config

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -13,18 +13,7 @@ var registered = false
 
 @KtorExperimentalAPI
 fun main() = runBlocking {
-    KotBot.create(listOf<suspend (ServerMessage) -> Unit>(
-            {
-                if (it.command == "PRIVMSG" && it.parameters[0] == "#discodevs" && it.parameters[1] == "hi") {
-                    logger.info("Someone said hi")
-                }
-            },
-            {
-                if (it.command == "PRIVMSG" && it.parameters[0] == "#discodevs" && it.parameters[1] == "bye") {
-                    logger.info("Someone said bye")
-                }
-            }
-    ))
+    KotBot.create()
 }
 
 object IrcConfig {
