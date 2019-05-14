d4f763972379d9385e884be98419326cfc026626
Nathan Kessler <nathant93@gmail.com>
2019-05-13 22:53:54 -0400

    Add TS definition for JS interface

---
+++ plugins/js/context.d.ts
@@ -0,0 +1,17 @@
+declare namespace KotBotEvent {
+    let message: String
+    let source: String
+    let command: String
+    let body: String
+    let name: String
+    function say(message: String)
+    function respond(message: String)
+}
+
+declare namespace context {
+    function addEventHandler(handler: (KotBotEvent) => void)
+    function say(location: String, message: String)
+    function request(url: String): String
+    function configString(key: String): String
+    function configInt(key: String): Number
+}
