6653362066db589db4d5152cc0894debb9f9ab77
Nathan Kessler <nathant93@gmail.com>
2019-05-21 09:13:02 -0400

    Add chicken_reply.py module

---
+++ plugins/python/chicken_reply.py
@@ -0,0 +1,15 @@
+responses = {'what' : 'chicken butt',
+             'why'  : 'chicken thigh',
+             'wat'  : 'chicken bat',
+             'where': 'chicken hair',
+             'when' : 'chicken pen',
+             'who'  : 'chicken poo',
+             'how'  : 'chicken plow'}
+
+def chicken_reply(event):
+    if event.message in responses.keys():
+        message = responses[event.message.lower()]
+        message = message.upper() if event.message.isupper() else message
+        event.respond(message)
+
+context.addEventHandler(chicken_reply)
