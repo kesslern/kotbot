0fa07ab7a83637ddcac003f190311e516d9b76fd
Nathan Kessler <nathant93@gmail.com>
2019-05-21 22:37:02 -0400

    Improve weather plugin & move to own file

--- plugins/js/test.js
+++ plugins/js/test.js
@@ -1,17 +1,9 @@
-context.addHelp("weather","Provide a zip code and get the weather")
 context.addHelp("remember", "Remember a key/value pair to be retrieved by recall")
 context.addHelp("recall", "Recall a key/value pair that was stored by remember")
 
 context.addEventHandler((event) => {
     if (event.message === "javascript") {
-        event.respond("hi from javascript")
-    }
-    if (event.command === "weather") {
-        const zip = event.body
-        const key = context.configString("openweathermap")
-        const weather = JSON.parse(context.request("http://api.openweathermap.org/data/2.5/weather?zip=" + zip + "&APPID=" + key))
-        const temp = ("" + ((weather.main.temp - 273.15) * 9 / 5 + 32)).slice(0, 4)
-        event.respond(`${weather.name} is ${weather.weather[0].main}, ${temp} degrees`)
+        event.say("hi from javascript")
     }
     if (event.command === "remember") {
         const str = event.body
---
+++ plugins/js/weather.js
@@ -0,0 +1,43 @@
+context.addHelp("weather","Provide a zip code and get the weather")
+
+const c2f = celsius => celsius * 9/5 + 32
+const k2c = kelvin => kelvin - 273.15
+const k2f = kelvin => c2f(k2c(kelvin))
+const formatNumber = num => `${num}`.slice(0, 4)
+
+const timeZone = context.configString("timezone")
+
+
+context.addEventHandler(event => {
+    const zip = event.body
+    const key = context.configString("openweathermap")
+    const weather = JSON.parse(context.request("http://api.openweathermap.org/data/2.5/weather?zip=" + zip + "&APPID=" + key))
+
+    const temp = {
+        currentF: formatNumber(k2f(weather.main.temp)),
+        maxF: formatNumber(k2f(weather.main.temp_max)),
+        minF: formatNumber(k2f(weather.main.temp_min)),
+        currentC: formatNumber(k2c(weather.main.temp)),
+        maxC: formatNumber(k2c(weather.main.temp_max)),
+        minC: formatNumber(k2c(weather.main.temp_min)),
+    }
+
+    const conditions = weather.weather[0].description
+    const humidity = weather.main.humidity
+    const wind = weather.wind.speed
+    const sunrise = new Date(weather.sys.sunrise * 1000).toLocaleTimeString('en-US', {timeZone, hour: 'numeric', minute: 'numeric', hour12: true})
+    const sunset =  new Date(weather.sys.sunset * 1000).toLocaleTimeString('en-US', {timeZone, hour: 'numeric', minute: 'numeric', hour12: true})
+
+    const currentWeather = [
+        `Current conditions in ${weather.name}: ${conditions}`,
+        `Current temperature: ${temp.currentF}F (${temp.currentC}C)`,
+        `Max temp: ${temp.maxF}F (${temp.maxC}C)`,
+        `Overnight min temp: ${temp.minF}F (${temp.minC}C)`,
+        `Humidity: ${humidity}/100`,
+        `Wind: ${wind}m/s`,
+        `Sunrise: ${sunrise}`,
+        `Sunset: ${sunset}`,
+    ]
+
+    event.respond(currentWeather.join(" | "))
+})
