410764ef7d8266d2ecdab4b0ac47e46027a55396
Nathan Kessler <nathant93@gmail.com>
2019-05-22 22:59:30 -0400

    Show top 10 of all time last.fm

--- plugins/js/lastfm.js
+++ plugins/js/lastfm.js
@@ -3,6 +3,18 @@ const apikey = context.configString('lastfm_api')
 context.addEventHandler(event => {
     if (event.command === 'lta') {
         const response = JSON.parse(context.request(`http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=${event.body}&api_key=${apikey}&format=json`))
-        event.respond(`Top artist: ${response.topartists.artist[0].name}`)
+        const artists = response.topartists.artist
+
+        const result = []
+        for (let i = 0; i < 10 && i < artists.length; i++) {
+            const artist = artists[i]
+            result.push({
+                artist: artist.name,
+                count: artist.playcount,
+            })
+         }
+
+        const top = result.map(it => `${it.artist} [${it.count}]`).join(' | ')
+        event.respond(`Top artists: ${top}`)
     }
 })
